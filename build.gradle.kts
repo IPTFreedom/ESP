088347f6239ca0baf08e6a0dcfdbc22971fcfed9
Allink <arclicious@vivaldi.net>
2023-05-02 21:39:33 +0100

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
+
+plugins {
+    id("java")
+    id("net.minecrell.plugin-yml.bukkit") version "0.5.3"
+}
+
+group = "com.github.allinkdev"
+version = "1.0-SNAPSHOT"
+
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+    maven("https://repo.scissors.gg/repository/scissors-snapshot/")
+    maven("https://repo.essentialsx.net/releases/")
+    maven("https://nexus-01.core.atlas-media.co.uk/repository/totalfreedom-development/")
+}
+
+dependencies {
+    compileOnly("me.totalfreedom.scissors:scissors-api:1.17.1-R0.1-SNAPSHOT")
+    compileOnly("net.essentialsx:EssentialsX:2.19.7")
+
+    compileOnly("me.totalfreedom:TotalFreedomMod:2021.09")
+}
+
+bukkit {
+    author = "videogamesm12"
+    description = "Temporary plugin that patches a known TPToggle bypass exploit in Essentials"
+    apiVersion = "1.17"
+    name = "ESP"
+    load = BukkitPluginDescription.PluginLoadOrder.STARTUP
+    main = "com.github.allinkdev.esp.Main"
+
+    depend = listOf("Essentials", "TotalFreedomMod")
+}
---
+++ src/main/java/com/github/allinkdev/esp/Main.java
@@ -0,0 +1,66 @@
+package com.github.allinkdev.esp;
+
+import com.earth2me.essentials.CommandSource;
+import com.earth2me.essentials.Essentials;
+import com.earth2me.essentials.User;
+import com.github.atlasmediagroup.scissors.event.player.SpectatorTeleportEvent;
+import me.totalfreedom.totalfreedommod.TotalFreedomMod;
+import me.totalfreedom.totalfreedommod.player.FPlayer;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import static com.earth2me.essentials.I18n.tl;
+
+public final class Main extends JavaPlugin implements Listener {
+    private TotalFreedomMod tfm;
+    private Essentials essentials;
+
+    @Override
+    public void onEnable() {
+        // The only safe place to get dependency plugins is in onEnable
+        this.tfm = TotalFreedomMod.getPlugin();
+        this.essentials = JavaPlugin.getPlugin(Essentials.class);
+
+        final PluginManager pluginManager = Bukkit.getPluginManager();
+
+        pluginManager.registerEvents(this, this);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    private void onSpectatorTeleport(final SpectatorTeleportEvent event) {
+        final Player player = event.getPlayer();
+        final boolean isAdmin = tfm.al.isAdmin(player);
+
+        // We want to allow admins to bypass TPToggle in spectator mode
+        if (isAdmin) {
+            return;
+        }
+
+        final Entity target = event.getTarget();
+
+        // We don't care if a player spectates, say, a cow
+        if (!(target instanceof final Player targetPlayer)) {
+            return;
+        }
+
+        final User essentialsUser = essentials.getUser(targetPlayer);
+
+        if (essentialsUser.isTeleportEnabled()) {
+            return;
+        }
+
+        final CommandSource commandSource = new CommandSource(player);
+        final String displayName = targetPlayer.getDisplayName();
+        final String message = tl("teleportDisabled", displayName);
+        final Exception exception = new Exception(message);
+
+        essentials.showError(commandSource, exception, "tp");
+        event.setCancelled(true);
+    }
+}
